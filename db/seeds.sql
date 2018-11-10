f298126daa0b121f553c748e810f3ec4737916c8
csdojo <sherrywanwang@gmail.com>
2018-11-10 13:36:04 -0500

    copy from eat da burger project

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Steak Burger');
+INSERT INTO burgers (name) VALUES ('Patty Melt');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ("McDonald's Big Mac");
+
+
+
